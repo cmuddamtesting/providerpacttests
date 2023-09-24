d88b52b9295652fce8009330ba60428ee8a0e553
sekhar.muddam <sekhar.muddam@gmail.com>
2023-09-24 18:27:17 +1000

    test11

--- pact-provider/PactProviderTests/PactProviderTests/ProviderPactTestsConstants.cs
+++ pact-provider/PactProviderTests/PactProviderTests/ProviderPactTestsConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class ProviderPactTestsConstants
     {
-        public const string ConsumerName = "Order-Consumer1";
-        public const string ProviderName = "Order-Provider1";
+        public const string ConsumerName = "Order-Consumer";
+        public const string ProviderName = "Order-Provider";
     }
 }
